dfa216b1303a8f0fc0e46b27182caa3eecf691d5
vikvillalba <vikkivillalba30@gmail.com>
2025-02-16 13:12:07 -0700

    quinto

---
+++ src/main/java/itson/ticketwizard/TicketWizard.java
@@ -0,0 +1,13 @@
+
+package itson.ticketwizard;
+
+/**
+ *
+ * @author victoria
+ */
+public class TicketWizard {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
