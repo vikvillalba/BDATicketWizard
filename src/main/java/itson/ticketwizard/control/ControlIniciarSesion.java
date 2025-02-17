92a8f094a8b8b7f67415e3616d9e921a5f645f51
vikvillalba <vikkivillalba30@gmail.com>
2025-02-17 13:54:22 -0700

    control inicio de sesion

---
+++ src/main/java/itson/ticketwizard/control/ControlIniciarSesion.java
@@ -0,0 +1,17 @@
+package itson.ticketwizard.control;
+import itson.ticketwizard.presentacion.FrmInicioSesion;
+
+/**
+ *
+ * @author victoria
+ */
+public class ControlIniciarSesion {
+    private FrmInicioSesion inicioSesion;
+    
+    public void iniciarSesion(){ // inicia el caso de uso
+        this.inicioSesion = new FrmInicioSesion(this);
+        this.inicioSesion.setVisible(true);
+    
+    } 
+            
+}
