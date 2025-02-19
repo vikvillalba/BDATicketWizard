9875479e43f6772c6682bfbe5f6295e937be84c5
vikvillalba <vikkivillalba30@gmail.com>
2025-02-18 23:33:48 -0700

    se le agregó la lógica a las clases DAO de direcciones y usuarios

--- src/main/java/itson/ticketwizard/persistencia/DireccionesDAO.java
+++ src/main/java/itson/ticketwizard/persistencia/DireccionesDAO.java
@@ -1,9 +1,49 @@
 package itson.ticketwizard.persistencia;
 
+import itson.ticketwizard.dtos.NuevoDomicilioUsuarioDTO;
+import itson.ticketwizard.entidades.DomicilioUsuario;
+import itson.ticketwizard.entidades.Usuario;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  *
  * @author victoria
  */
 public class DireccionesDAO {
     
+    private final ManejadorConexiones manejadorConexiones;
+
+    public DireccionesDAO(ManejadorConexiones manejadorConexiones) {
+        this.manejadorConexiones = manejadorConexiones;
+    }
+
+    public DomicilioUsuario registrarDireccion(NuevoDomicilioUsuarioDTO nuevoDomicilioDTO, Usuario usuario){
+        String comandoSQL = """
+                            INSERT INTO DOMICILIOSUSUARIOS (CODIGOUSUARIO, CALLE, NUMERO, COLONIA, CIUDAD, ESTADO, CODIGOPOSTAL)
+                            VALUES(?, ?, ?, ?, ?, ?, ?);
+                            """;
+        
+        try {
+            Connection conexion = manejadorConexiones.crearConexion();
+            PreparedStatement comando = conexion.prepareStatement(comandoSQL);
+            
+            comando.setInt(1, usuario.getCodigoUsuario());
+            comando.setString(2, nuevoDomicilioDTO.getCalle());
+            comando.setString(3, nuevoDomicilioDTO.getNumero());
+            comando.setString(4, nuevoDomicilioDTO.getColonia());
+            comando.setString(5, nuevoDomicilioDTO.getCiudad());
+            comando.setString(6, nuevoDomicilioDTO.getEstado());
+            comando.setInt(7, nuevoDomicilioDTO.getCodigoPostal());
+            
+            int filasAfectadas = comando.executeUpdate();
+            System.out.println("se registró el domicilio");
+            
+        }catch(SQLException ex){
+            
+            System.err.println(ex.getMessage()); 
+        }
+        return null;
+    }
 }
--- src/main/java/itson/ticketwizard/persistencia/UsuariosDAO.java
+++ src/main/java/itson/ticketwizard/persistencia/UsuariosDAO.java
@@ -1,16 +1,65 @@
 package itson.ticketwizard.persistencia;
 
+import itson.ticketwizard.dtos.NuevoUsuarioDTO;
+import itson.ticketwizard.entidades.Usuario;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author victoria
  */
 public class UsuariosDAO { // almacena usuarios en la bd
-    
+
     private final ManejadorConexiones manejadorConexiones;
 
     public UsuariosDAO(ManejadorConexiones manejadorConexiones) {
         this.manejadorConexiones = manejadorConexiones;
     }
-    
-    
+
+    public Usuario registrarUsuario(NuevoUsuarioDTO nuevoUsuarioDTO) {
+        String codigoSQL = """
+                           INSERT INTO USUARIOS (NOMBRES, APELLIDOPATERNO, APELLIDOMATERNO, FECHANACIMIENTO, 
+                           NOMBREUSUARIO, CONTRASENA, CORREOELECTRONICO)
+                           VALUES (?, ?, ?, ?, ?, ?, ?);
+                           """;
+
+        try {
+            Connection conexion = manejadorConexiones.crearConexion();
+            PreparedStatement comando = conexion.prepareStatement(codigoSQL, Statement.RETURN_GENERATED_KEYS);
+
+            comando.setString(1, nuevoUsuarioDTO.getNombres());
+            comando.setString(2, nuevoUsuarioDTO.getApellidoPaterno());
+            comando.setString(3, nuevoUsuarioDTO.getApellidoMaterno());
+            comando.setDate(4, new java.sql.Date(nuevoUsuarioDTO.getFechaNacimiento().getTime()));
+            comando.setString(5, nuevoUsuarioDTO.getNombreUsuario());
+            comando.setString(6, nuevoUsuarioDTO.getContrasenia());
+            comando.setString(7, nuevoUsuarioDTO.getCorreoElectronico());
+
+            int filasAfectadas = comando.executeUpdate();
+            if (filasAfectadas > 0) {
+                try (ResultSet generatedKeys = comando.getGeneratedKeys()) {
+                    if (generatedKeys.next()) {
+                        int idGenerado = generatedKeys.getInt(1); // Obtener el ID generado por el sgbd
+
+                        // Retornar el objeto Usuario con el ID asignado
+                        return new Usuario(idGenerado,
+                                nuevoUsuarioDTO.getNombres(), nuevoUsuarioDTO.getApellidoPaterno(), nuevoUsuarioDTO.getApellidoPaterno(),
+                                nuevoUsuarioDTO.getFechaNacimiento(), 0.0f, nuevoUsuarioDTO.getNombreUsuario(),
+                                nuevoUsuarioDTO.getContrasenia(), nuevoUsuarioDTO.getCorreoElectronico());
+                    }
+                }
+            }
+            System.out.println("se registró el usuario");
+
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
+        return null;
+    }
+
 }
