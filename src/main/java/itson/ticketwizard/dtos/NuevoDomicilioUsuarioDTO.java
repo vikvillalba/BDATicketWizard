044ad0fd477e8bc39011b630ab1d27ffced4d8aa
vikvillalba <vikkivillalba30@gmail.com>
2025-02-18 23:34:13 -0700

    clases DTO para nuevo domicilio y nuevo usuario

--- src/main/java/itson/ticketwizard/dtos/NuevoDomicilioUsuarioDTO.java
+++ src/main/java/itson/ticketwizard/dtos/NuevoDomicilioUsuarioDTO.java
@@ -5,7 +5,6 @@ package itson.ticketwizard.dtos;
  * @author victoria
  */
 public class NuevoDomicilioUsuarioDTO {
-    private final Integer codigoUsuario;
     private final String calle;
     private final String numero;
     private final String colonia;
@@ -13,8 +12,7 @@ public class NuevoDomicilioUsuarioDTO {
     private final String estado;
     private final Integer codigoPostal;
 
-    public NuevoDomicilioUsuarioDTO(Integer codigoUsuario, String calle, String numero, String colonia, String ciudad, String estado, Integer codigoPostal) {
-        this.codigoUsuario = codigoUsuario;
+    public NuevoDomicilioUsuarioDTO( String calle, String numero, String colonia, String ciudad, String estado, Integer codigoPostal) {
         this.calle = calle;
         this.numero = numero;
         this.colonia = colonia;
@@ -23,10 +21,6 @@ public class NuevoDomicilioUsuarioDTO {
         this.codigoPostal = codigoPostal;
     }
     
-
-    public int getCodigoUsuario() {
-        return codigoUsuario;
-    }
 
     public String getCalle() {
         return calle;
--- src/main/java/itson/ticketwizard/dtos/NuevoUsuarioDTO.java
+++ src/main/java/itson/ticketwizard/dtos/NuevoUsuarioDTO.java
@@ -7,16 +7,23 @@ import java.util.Date;
  * @author victoria 
  */
 public class NuevoUsuarioDTO {
+
     private String nombres;
     private String apellidoPaterno;
     private String apellidoMaterno;
     private Date fechaNacimiento;
+    private String nombreUsuario;
+    private String contrasenia;
+    private String correoElectronico;
 
-    public NuevoUsuarioDTO(String nombres, String apellidoPaterno, String apellidoMaterno, Date fechaNacimiento) {
+    public NuevoUsuarioDTO(String nombres, String apellidoPaterno, String apellidoMaterno, Date fechaNacimiento, String nombreUsuario, String contrasenia, String correoElectronico) {
         this.nombres = nombres;
         this.apellidoPaterno = apellidoPaterno;
         this.apellidoMaterno = apellidoMaterno;
         this.fechaNacimiento = fechaNacimiento;
+        this.nombreUsuario = nombreUsuario;
+        this.contrasenia = contrasenia;
+        this.correoElectronico = correoElectronico;
     }
 
     public String getNombres() {
@@ -34,6 +41,19 @@ public class NuevoUsuarioDTO {
     public Date getFechaNacimiento() {
         return fechaNacimiento;
     }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
     
     
     
