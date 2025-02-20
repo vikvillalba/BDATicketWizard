31ee87f70a751026f6087f85aa8bb1dce019747a
saulapo18 <saul.apodaca252020@potros.itson.edu.mx>
2025-02-20 01:13:35 -0700

    avance y modificaciones de DepositosDAO

--- src/main/java/itson/ticketwizard/persistencia/DepositosDAO.java
+++ src/main/java/itson/ticketwizard/persistencia/DepositosDAO.java
@@ -1,5 +1,14 @@
 package itson.ticketwizard.persistencia;
 
+import itson.ticketwizard.dtos.NuevoDepositoDTO;
+import itson.ticketwizard.entidades.Deposito;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+
 /**
  *
  * @author victoria
@@ -11,5 +20,38 @@ public class DepositosDAO {
         this.manejadorConexiones = manejadorConexiones;
     }
      
+    public Deposito realizarDeposito(NuevoDepositoDTO nuevoDepositoDTO, int codigoUsuario) throws SQLException{
+        String codigoSQL="""
+                         INSET INTO DEPOSITOS(CODIGOUSUARIO, MONTO, FECHAHORA)
+                         VALUES(?,?,?);
+                         """;
+        try{
+            Connection conexion = manejadorConexiones.crearConexion();
+            PreparedStatement comando = conexion.prepareStatement(codigoSQL);
+            
+            comando.setInt(1, codigoUsuario);
+            
+            comando.setFloat(2,nuevoDepositoDTO.getSaldo());
+            
+            Timestamp fechaHora = new Timestamp(System.currentTimeMillis());
+            comando.setTimestamp(3, fechaHora);
+           
+         int filasAfectadas = comando.executeUpdate();
+            
+            if (filasAfectadas > 0) {
+                try (ResultSet generatedKeys = comando.getGeneratedKeys()) {
+                    if (generatedKeys.next()) {
+                        int codigoDeposito = generatedKeys.getInt(1);
+                        
+                        return new Deposito(codigoDeposito, codigoUsuario, nuevoDepositoDTO.getSaldo(), fechaHora.toLocalDateTime());
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            System.err.println("Error al realizar el depósito: " + e.getMessage());
+            throw e;
+        }
+        return null;
+    }
      
 }
