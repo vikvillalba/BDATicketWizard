8a099f9752fe5a3d440c01cdb0cdbf6bd254059a
vikvillalba <vikkivillalba30@gmail.com>
2025-02-21 01:45:05 -0700

    ....

--- src/main/java/itson/ticketwizard/persistencia/UsuariosDAO.java
+++ src/main/java/itson/ticketwizard/persistencia/UsuariosDAO.java
@@ -6,6 +6,7 @@ import itson.ticketwizard.dtos.UsuarioRegistradoDTO;
 import itson.ticketwizard.entidades.Usuario;
 import java.math.BigDecimal;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
