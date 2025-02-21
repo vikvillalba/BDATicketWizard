f1e81c25edc064a65cb8e81eaae3c3fba6b4d032
saulapodaca <saul.apodaca252020@potros.itson.edu.mx>
2025-02-21 13:05:04 -0700

    depositosDao terminado

--- src/main/java/itson/ticketwizard/persistencia/DepositosDAO.java
+++ src/main/java/itson/ticketwizard/persistencia/DepositosDAO.java
@@ -4,12 +4,14 @@ import itson.ticketwizard.dtos.NuevoDepositoDTO;
 import itson.ticketwizard.dtos.UsuarioRegistradoDTO;
 import itson.ticketwizard.entidades.Deposito;
 import java.math.BigDecimal;
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Timestamp;
+import java.sql.Types;
 import java.time.LocalDateTime;
 
 /**
@@ -24,24 +26,24 @@ public class DepositosDAO {
     }
      
     public Deposito realizarDeposito(NuevoDepositoDTO nuevoDepositoDTO, UsuarioRegistradoDTO usuarioRegistradoDTO) throws SQLException, PersistenciaException{
-        String codigoSQL="""
-                         CALL GESTIONAR_TRANSACCION(?,?,?,?,?,?);
-                         """;
-        try{
-            Connection conexion = manejadorConexiones.crearConexion();
-            PreparedStatement comando = conexion.prepareStatement(codigoSQL);
-            
+        try(Connection conexion = manejadorConexiones.crearConexion();
+            CallableStatement comando = conexion.prepareCall("{CALL GESTIONAR_TRANSACCION(?,?,?,?,?,?)}")){
+            //se usa CallableStatement para procedimientos almacenados
             comando.setString(1, "DEPOSITO");
             comando.setInt(2, usuarioRegistradoDTO.getCodigoUsuario());
-            comando.setString(3, null);
+            comando.setNull(3, Types.VARCHAR);
             comando.setBigDecimal(4, nuevoDepositoDTO.getSaldo());
-            comando.setInt(5, 0);
-            comando.setDate(6, null);
-            System.out.println(comando.executeUpdate() > 0);
+            comando.setNull(5, Types.INTEGER);
+            comando.setNull(6, Types.TIMESTAMP);
+            // para mandar los valores null se utiliza el setNull, se le manda el lugar y el tipo de dato que es
+            
+            int filasAfectadas = comando.executeUpdate();
+            System.out.println("Filas afectadas: " + filasAfectadas);
+
             return new Deposito(usuarioRegistradoDTO.getCodigoUsuario(), nuevoDepositoDTO.getSaldo(), LocalDateTime.now());
 
         }catch(SQLException e){
-            System.err.println(e.getMessage());
+            System.err.println("ERROR SQL:" + e.getMessage());
             throw new PersistenciaException("ERROR: ERROR AL REALIZAR LA TRANSACCION");
         }
      
