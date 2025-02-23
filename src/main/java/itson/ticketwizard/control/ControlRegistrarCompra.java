351353b9d54f56ce150b403f97ca5b2aa30703a9
vikvillalba <vikkivillalba30@gmail.com>
2025-02-23 14:17:33 -0700

    se agregó la funcionalidad para apartar boletos cuando el usuario no tiene saldo suficiente

--- src/main/java/itson/ticketwizard/control/ControlRegistrarCompra.java
+++ src/main/java/itson/ticketwizard/control/ControlRegistrarCompra.java
@@ -1,14 +1,17 @@
 package itson.ticketwizard.control;
 
+import itson.ticketwizard.dtos.BoletoApartadoDTO;
 import itson.ticketwizard.dtos.BoletoCompraDTO;
 import itson.ticketwizard.dtos.BoletoDTO;
 import itson.ticketwizard.dtos.NuevaCompraDTO;
 import itson.ticketwizard.dtos.UsuarioRegistradoDTO;
+import itson.ticketwizard.persistencia.BoletosApartadosDAO;
 import itson.ticketwizard.persistencia.BoletosDAO;
 import itson.ticketwizard.persistencia.ComprasDAO;
 import itson.ticketwizard.persistencia.PersistenciaException;
 import itson.ticketwizard.persistencia.UsuariosDAO;
 import itson.ticketwizard.presentacion.FrmDetallesBoletoCompra;
+import java.time.LocalDateTime;
 import javax.swing.JOptionPane;
 
 /** Control que maneja el flujo para el caso de uso de registrar la compra de boletos, así como mostrar los boletos adquiridos.
@@ -21,11 +24,13 @@ public class ControlRegistrarCompra {
     private final BoletosDAO boletosDAO;
     private final ComprasDAO comprasDAO;
     private UsuariosDAO usuariosDAO;
+    private BoletosApartadosDAO boletosApartadosDAO;
 
-    public ControlRegistrarCompra(BoletosDAO boletosDAO, ComprasDAO comprasDAO, UsuariosDAO usuariosDAO) {
+    public ControlRegistrarCompra(BoletosDAO boletosDAO, ComprasDAO comprasDAO, UsuariosDAO usuariosDAO, BoletosApartadosDAO boletosApartadosDAO) {
         this.boletosDAO = boletosDAO;
         this.comprasDAO = comprasDAO;
         this.usuariosDAO = usuariosDAO;
+        this.boletosApartadosDAO = boletosApartadosDAO;
     }
     
     public void mostrarDetallesBoletoCompra(UsuarioRegistradoDTO usuarioRegistradoDTO, BoletoCompraDTO boletoCompraDTO){
@@ -51,7 +56,10 @@ public class ControlRegistrarCompra {
             // Comprobar si el usuario tiene suficiente saldo
             if (usuarioConSaldo.getSaldo().compareTo(boletoDTO.getPrecio()) < 0) {
                 System.out.println("No hay dinero suficiente para comprar el boleto.");
-                // se aparta el boleto ????
+               
+                BoletoApartadoDTO boletoApartadoDTO = new BoletoApartadoDTO(usuarioRegistradoDTO.getCodigoUsuario(), boletoDTO.getCodigoBoleto(), boletoDTO.getNumeroSerie(), LocalDateTime.now(), boletoDTO.getPrecio());               
+                this.boletosApartadosDAO.apartarBoleto(boletoApartadoDTO, usuarioRegistradoDTO);
+                this.mostrarMensajeBoletoApartado();
                 return;
             }
             NuevaCompraDTO nuevaCompraDTO = new NuevaCompraDTO(usuarioRegistradoDTO.getCodigoUsuario(), boletoDTO.getNumeroSerie(), boletoDTO.getPrecio());
@@ -75,6 +83,16 @@ public class ControlRegistrarCompra {
 
     private void mostrarMensajeCompraRealizada() {
         JOptionPane.showMessageDialog(detallesBoletoCompra, "¡Compra exitosa!", "Información", JOptionPane.INFORMATION_MESSAGE);
+        detallesBoletoCompra.dispose();
+    }
+    
+    private void mostrarMensajeBoletoApartado() {
+        JOptionPane.showMessageDialog(detallesBoletoCompra,
+                "Saldo insuficiente.\nEl boleto se ha apartado por 10 minutos.\n"
+                + "Deposite saldo antes del periodo de tiempo o el boleto se liberará a la venta otra vez.",
+                "Información",
+                JOptionPane.INFORMATION_MESSAGE);
+
         detallesBoletoCompra.dispose();
     }
 }
