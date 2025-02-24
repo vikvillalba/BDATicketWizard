28d443e86f7b514f032b1201a92897119d1abec4
vikvillalba <vikkivillalba30@gmail.com>
2025-02-23 18:44:29 -0700

    se eliminó DomiciliosDAO sus métodos se implementaron en UsuariosDAO ya que los domicilios se crean al mismo tiempo que los usuarios.

--- src/main/java/itson/ticketwizard/persistencia/UsuariosDAO.java
+++ src/main/java/itson/ticketwizard/persistencia/UsuariosDAO.java
@@ -1,7 +1,6 @@
 package itson.ticketwizard.persistencia;
 
-import itson.ticketwizard.dtos.NuevoDomicilioUsuarioDTO;
-import itson.ticketwizard.dtos.NuevoUsuarioDTO;
+import itson.ticketwizard.dtos.UsuarioDTO;
 import itson.ticketwizard.dtos.UsuarioRegistradoDTO;
 import itson.ticketwizard.entidades.Usuario;
 import java.math.BigDecimal;
@@ -11,6 +10,7 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.LinkedList;
 import java.util.List;
 import org.mindrot.jbcrypt.BCrypt;
@@ -27,49 +27,49 @@ public class UsuariosDAO { // almacena usuarios en la bd
         this.manejadorConexiones = manejadorConexiones;
     }
 
-public Usuario registrarUsuario(NuevoUsuarioDTO nuevoUsuarioDTO, NuevoDomicilioUsuarioDTO nuevoDomicilioDTO) {
-    String codigoSQL = "{CALL SP_crearCuenta(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)}"; 
+    public Usuario registrarUsuario(UsuarioDTO nuevoUsuarioDTO) {
+        String codigoSQL = "{CALL SP_crearCuenta(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)}";
 
+        try {
+            Connection conexion = manejadorConexiones.crearConexion();
+            CallableStatement comando = conexion.prepareCall(codigoSQL);
 
-    try {
-        Connection conexion = manejadorConexiones.crearConexion();
-        CallableStatement comando = conexion.prepareCall(codigoSQL);
+            comando.setString(1, nuevoUsuarioDTO.getNombres());
+            comando.setString(2, nuevoUsuarioDTO.getApellidoPaterno());
+            comando.setString(3, nuevoUsuarioDTO.getApellidoMaterno());
+            comando.setDate(4, java.sql.Date.valueOf(nuevoUsuarioDTO.getFechaNacimiento()));
 
-        comando.setString(1, nuevoUsuarioDTO.getNombres());
-        comando.setString(2, nuevoUsuarioDTO.getApellidoPaterno());
-        comando.setString(3, nuevoUsuarioDTO.getApellidoMaterno());
-        comando.setDate(4, new java.sql.Date(nuevoUsuarioDTO.getFechaNacimiento().getTime()));
-        comando.setString(5, nuevoUsuarioDTO.getNombreUsuario());
-        String contraseniaEncriptada = BCrypt.hashpw(nuevoUsuarioDTO.getContrasenia(), BCrypt.gensalt());
-        comando.setString(6, contraseniaEncriptada);
+            comando.setString(5, nuevoUsuarioDTO.getNombreUsuario());
+            String contraseniaEncriptada = BCrypt.hashpw(nuevoUsuarioDTO.getContrasenia(), BCrypt.gensalt());
+            comando.setString(6, contraseniaEncriptada);
 
-        comando.setString(7, nuevoUsuarioDTO.getCorreoElectronico());
-        comando.setString(8, nuevoDomicilioDTO.getCalle());
-        comando.setString(9, nuevoDomicilioDTO.getNumero());
-        comando.setString(10, nuevoDomicilioDTO.getColonia());
-        comando.setString(11, nuevoDomicilioDTO.getCiudad());
-        comando.setString(12, nuevoDomicilioDTO.getEstado());
-        comando.setInt(13, nuevoDomicilioDTO.getCodigoPostal());
+            comando.setString(7, nuevoUsuarioDTO.getCorreoElectronico());
+            comando.setString(8, nuevoUsuarioDTO.getCalle());
+            comando.setString(9, nuevoUsuarioDTO.getNumero());
+            comando.setString(10, nuevoUsuarioDTO.getColonia());
+            comando.setString(11, nuevoUsuarioDTO.getCiudad());
+            comando.setString(12, nuevoUsuarioDTO.getEstado());
+            comando.setInt(13, nuevoUsuarioDTO.getCodigoPostal());
 
-        comando.registerOutParameter(14, java.sql.Types.INTEGER);
-        comando.execute();
+            comando.registerOutParameter(14, java.sql.Types.INTEGER);
+            comando.execute();
 
-        // Obtener el ID generado
-        int idGenerado = comando.getInt(14);
+            // Obtener el ID generado
+            int idGenerado = comando.getInt(14);
 
-        if (idGenerado > 0) {
+            if (idGenerado > 0) {
 
-            return new Usuario(idGenerado,
-                    nuevoUsuarioDTO.getNombres(), nuevoUsuarioDTO.getApellidoPaterno(), nuevoUsuarioDTO.getApellidoMaterno(),
-                    nuevoUsuarioDTO.getFechaNacimiento(), 0.0f, nuevoUsuarioDTO.getNombreUsuario(),
-                    contraseniaEncriptada, nuevoUsuarioDTO.getCorreoElectronico());
+                return new Usuario(idGenerado,
+                        nuevoUsuarioDTO.getNombres(), nuevoUsuarioDTO.getApellidoPaterno(), nuevoUsuarioDTO.getApellidoMaterno(),
+                        nuevoUsuarioDTO.getFechaNacimiento(), 0.0f, nuevoUsuarioDTO.getNombreUsuario(),
+                        contraseniaEncriptada, nuevoUsuarioDTO.getCorreoElectronico());
+            }
+
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
         }
-
-    } catch (SQLException ex) {
-        System.err.println(ex.getMessage());
+        return null;
     }
-    return null;
-}
 
     // obtener usuarios existentes en la bd
     public List<UsuarioRegistradoDTO> ObtenerCuentasExistentes() {
@@ -130,35 +130,133 @@ public Usuario registrarUsuario(NuevoUsuarioDTO nuevoUsuarioDTO, NuevoDomicilioU
         return null;
     }
     
-    
     //actualiza los datos del usuario
-    public boolean actualizarUsuario(NuevoUsuarioDTO usuarioActualizado, NuevoDomicilioUsuarioDTO domicilioActualizado) {
-    String sql = """
-                 UPDATE USUARIOS
-                 SET NOMBRES = ?, APELLIDOPATERNO = ?, APELLIDOMATERNO = ?,
-                 FECHANACIMIENTO = ?, CONTRASENA = ?, CORREOELECTRONICO = ?
-                 WHERE NOMBREUSUARIO = ?;
-                 """;
+    public boolean actualizarUsuario(UsuarioDTO usuarioActualizado, UsuarioRegistradoDTO usuarioRegistradoDTO) {
+        String usuariosSQL = """
+                        UPDATE USUARIOS
+                        SET NOMBRES = ?, APELLIDOPATERNO = ?, APELLIDOMATERNO = ?,
+                        FECHANACIMIENTO = ?, NOMBREUSUARIO = ?, CONTRASENA = ?, CORREOELECTRONICO = ?
+                        WHERE CODIGOUSUARIO = ?;
+                        """;
 
-    try (Connection conexion = manejadorConexiones.crearConexion();
-         PreparedStatement comando = conexion.prepareStatement(sql)) {
+        String domiciliosSQL = """
+                        UPDATE DOMICILIOSUSUARIOS
+                        SET CALLE = ?, NUMERO = ?, COLONIA = ?, CIUDAD = ?,
+                        ESTADO = ?, CODIGOPOSTAL = ?
+                        WHERE CODIGOUSUARIO = ?;
+                        """;
 
-        comando.setString(1, usuarioActualizado.getNombres());
-        comando.setString(2, usuarioActualizado.getApellidoPaterno());
-        comando.setString(3, usuarioActualizado.getApellidoMaterno());
-        comando.setDate(4, new Date(usuarioActualizado.getFechaNacimiento().getTime()));
-        //encripta la nueva contrasena
-        String contraseniaEncriptada = BCrypt.hashpw(usuarioActualizado.getContrasenia(), BCrypt.gensalt());
-        comando.setString(5, contraseniaEncriptada);
-        comando.setString(6, usuarioActualizado.getCorreoElectronico());
-        comando.setString(7, usuarioActualizado.getNombreUsuario());
-        //devuelve true si al menos 1 fila fue actualizada
-        return comando.executeUpdate() > 0;
+        Connection conexion = null;
 
-    } catch (SQLException ex) {
-        System.err.println("Error al actualizar usuario: " + ex.getMessage());
+        try {
+            conexion = manejadorConexiones.crearConexion();
+            conexion.setAutoCommit(false);
+
+            try (PreparedStatement comandoUsuario = conexion.prepareStatement(usuariosSQL)) {
+                comandoUsuario.setString(1, usuarioActualizado.getNombres());
+                comandoUsuario.setString(2, usuarioActualizado.getApellidoPaterno());
+                comandoUsuario.setString(3, usuarioActualizado.getApellidoMaterno());
+                comandoUsuario.setDate(4, java.sql.Date.valueOf(usuarioActualizado.getFechaNacimiento()));
+                comandoUsuario.setString(5, usuarioActualizado.getNombreUsuario());
+                comandoUsuario.setString(6, usuarioActualizado.getContrasenia());
+                comandoUsuario.setString(7, usuarioActualizado.getCorreoElectronico());
+                comandoUsuario.setInt(8, usuarioRegistradoDTO.getCodigoUsuario());
+
+                int filasUsuario = comandoUsuario.executeUpdate();
+                if (filasUsuario == 0) {
+                    throw new SQLException("No se encontró el usuario para actualizar.");
+                }
+            }
+
+            try (PreparedStatement comandoDomicilio = conexion.prepareStatement(domiciliosSQL)) {
+                comandoDomicilio.setString(1, usuarioActualizado.getCalle());
+                comandoDomicilio.setString(2, usuarioActualizado.getNumero());
+                comandoDomicilio.setString(3, usuarioActualizado.getColonia());
+                comandoDomicilio.setString(4, usuarioActualizado.getCiudad());
+                comandoDomicilio.setString(5, usuarioActualizado.getEstado());
+                comandoDomicilio.setInt(6, usuarioActualizado.getCodigoPostal());
+                comandoDomicilio.setInt(7, usuarioRegistradoDTO.getCodigoUsuario());
+
+                int filasDomicilio = comandoDomicilio.executeUpdate();
+                if (filasDomicilio == 0) {
+                    throw new SQLException("No se encontró el domicilio para actualizar.");
+                }
+            }
+
+            conexion.commit();
+            return true;
+
+        } catch (SQLException ex) {
+            System.err.println("Error al actualizar usuario y/o domicilio: " + ex.getMessage());
+            if (conexion != null) {
+                try {
+                    conexion.rollback();
+                } catch (SQLException rollbackEx) {
+                    System.err.println("Error al hacer rollback: " + rollbackEx.getMessage());
+                }
+            }
+        } finally {
+            if (conexion != null) {
+                try {
+                    conexion.close();
+                } catch (SQLException closeEx) {
+                    System.err.println("Error al cerrar conexión: " + closeEx.getMessage());
+                }
+            }
+        }
         return false;
     }
-}
+
+
+
+    public UsuarioDTO obtenerUsuarioDTO(UsuarioRegistradoDTO usuarioRegistradoDTO) throws PersistenciaException {
+        
+        String codigoSQL = """
+                           SELECT u.NOMBRES, u.APELLIDOPATERNO, u.APELLIDOMATERNO, u.FECHANACIMIENTO, 
+                           u.SALDODISPONIBLE, u.NOMBREUSUARIO, u.CONTRASENA, u.CORREOELECTRONICO, 
+                           d.CALLE, d.NUMERO, d.COLONIA, d.CIUDAD, d.ESTADO, d.CODIGOPOSTAL
+                           FROM USUARIOS u
+                           INNER JOIN DOMICILIOSUSUARIOS d ON d.CODIGOUSUARIO = u.CODIGOUSUARIO
+                           WHERE u.CODIGOUSUARIO = ?
+                           """;
+        
+        try{
+            Connection conexion = manejadorConexiones.crearConexion();
+            PreparedStatement comando = conexion.prepareStatement(codigoSQL);
+            comando.setInt(1, usuarioRegistradoDTO.getCodigoUsuario());
+            
+             ResultSet resultado = comando.executeQuery();
+            if (resultado.next()) {
+
+                String nombre = resultado.getString("nombres");
+                String apellidoPat = resultado.getString("apellidopaterno");
+                String apellidoMat = resultado.getString("apellidomaterno");
+                Date fechaSQL = resultado.getDate("fechanacimiento"); // Obtener como java.sql.Date
+                LocalDate fechaNacimiento = fechaSQL.toLocalDate();
+                BigDecimal saldo = resultado.getBigDecimal("saldodisponible");
+                String nombreUsuario = resultado.getString("nombreusuario");
+                String contrasena = resultado.getString("contrasena");
+                String correo = resultado.getString("correoelectronico");
+                String calle = resultado.getString("calle");
+                String numero = resultado.getString("numero");
+                String colonia = resultado.getString("colonia");
+                String ciudad = resultado.getString("ciudad");
+                String estado = resultado.getString("estado");
+                Integer codigoPostal = resultado.getInt("codigopostal");
+
+                return new UsuarioDTO(nombre, apellidoPat, apellidoMat,
+                        fechaNacimiento, nombreUsuario, contrasena, correo, calle, numero,
+                        colonia, ciudad, estado, codigoPostal);
+
+            }
+        
+        } catch (SQLException ex) {
+            System.err.println("Error al recuperar al usuario: " + ex.getMessage());
+            throw new PersistenciaException("Error al recuperar al usuario.");
+            
+        }
+    
+        return null;
+    }
 
 }
